1a3018db30fd0037c884001699256c399770bbb4
Michael Stringer <stringbean@users.noreply.github.com>
2020-09-05 20:25:18 +0100

    Document SystemStatus

--- src/SystemStatus.ts
+++ src/SystemStatus.ts
@@ -1,12 +1,38 @@
 import ApiSystem from './api/responses/SystemStatus';
 import { temperatureFromApi } from './utils';
 
+/**
+ * Current status of a Wiser system.
+ */
 export class SystemStatus {
+  /**
+   * Software version of the HeatHub.
+   */
   readonly version: string;
+
+  /**
+   * Whether Eco mode is enabled.
+   */
   readonly ecoMode: boolean;
+
+  /**
+   * Whether Away mode is active.
+   */
   readonly awayMode: boolean;
+
+  /**
+   * Set point temperature of Away mode.
+   */
   readonly awayModeSetPoint: number;
+
+  /**
+   * Whether the physical heating override button has been pressed.
+   */
   readonly heatingOverrideEnabled: boolean;
+
+  /**
+   * Whether any rooms have overrides set.
+   */
   readonly overridesActive: boolean;
 
   constructor(json: ApiSystem) {
